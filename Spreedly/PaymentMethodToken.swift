22ef7384b597a0ea56efd73b0d2cf41c9a84d88f
David Santoso <david@spreedly.com>
2015-10-10 12:09:31 -0400

    Abstract credit card for future extensibility

---
+++ Spreedly/PaymentMethodToken.swift
@@ -0,0 +1,13 @@
+//
+//  PaymentMethodToken.swift
+//  Spreedly
+//
+//  Created by David Santoso on 10/8/15.
+//  Copyright © 2015 Spreedly Inc. All rights reserved.
+//
+
+import Foundation
+
+public struct PaymentMethodToken {
+    var token: String
+}
--- Spreedly/SpreedlyAPIClient.swift
+++ Spreedly/SpreedlyAPIClient.swift
@@ -1,37 +1,39 @@
 //
 //  SpreedlyAPIClient.swift
+//  Spreedly
 //
 //  Created by David Santoso on 9/22/15.
 //  Copyright © 2015 Spreedly Inc. All rights reserved.
 //
 
 import Foundation
+import Spreedly
 
-public class SpreedlyAPIClient: NSObject {
+public class SpreedlyAPIClient {
     let environmentKey: String
+    
+    public typealias SpreedlyAPICompletionBlock = (token: String?, response: NSURLResponse?, error: NSError?) -> Void
 
     public init(environmentKey: String) {
         self.environmentKey = environmentKey
-        super.init()
+    }
+    
+    public func createPaymentMethodTokenWithCreditCard(creditCard: CreditCard, completion: SpreedlyAPICompletionBlock) {
+        let data = SpreedlyAPIRequestFormatter.format(creditCard)
+        self.createPaymentMethodTokenWithData(data, completion: completion)
     }
 
-    public func createPaymentMethodToken(cardData: [String: String], completion: (token: String?, error: NSError?) -> Void) {
+    func createPaymentMethodTokenWithData(data: NSData, completion: SpreedlyAPICompletionBlock) {
         let url = NSURL(string: "http://core.spreedly.dev/v1/payment_methods.json?environment_key=\(self.environmentKey)")
-        let body = [ "payment_method": [ "credit_card": cardData ]]
 
         let request = NSMutableURLRequest(URL: url!)
         let session = NSURLSession.sharedSession()
 
+        request.HTTPBody = data
         request.HTTPMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-        do {
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(body, options: [])
-        } catch let serializeError as NSError {
-            completion(token: nil, error: serializeError)
-        }
-
+        
         let task = session.dataTaskWithRequest(request) { data, response, error -> Void in
             guard data != nil else {
                 print("No data returned. Error: \(error)")
@@ -45,14 +47,14 @@ public class SpreedlyAPIClient: NSObject {
                             if let theToken = paymentMethod["token"] as? String {
                                 print("Before the completion block")
                                 dispatch_async(dispatch_get_main_queue(), {
-                                    completion(token: theToken, error: nil)
+                                    completion(token: theToken, response: response, error: nil)
                                 })
                             }
                         }
                     }
                 }
             } catch let parseError as NSError {
-                completion(token: nil, error: parseError)
+                completion(token: nil, response: response, error: parseError)
             }
         }
 
