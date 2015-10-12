f153a6b07014ecad39e05c2e114f337dd4f60d9d
David Santoso <david@spreedly.com>
2015-10-12 10:35:13 -0400

    Add public initializer for CreditCard

--- Spreedly/CreditCard.swift
+++ Spreedly/CreditCard.swift
@@ -11,22 +11,22 @@ import Foundation
 public class CreditCard {
     var firstName, lastName, number, verificationValue, month, year: String?
     var address1, address2, city, state, zip, country, phoneNumber: String?
-//    
-//    public init() {}
-//
-//    public init(attributes: [String: String]) {
-//        self.firstName = attributes["firstName"]
-//        self.lastName = attributes["lastName"]
-//        self.number = attributes["number"]
-//        self.verificationValue = attributes["verificationValue"]
-//        self.month = attributes["month"]
-//        self.year = attributes["year"]
-//        self.address1 = attributes["address1"]
-//        self.address2 = attributes["address2"]
-//        self.city = attributes["city"]
-//        self.state = attributes["state"]
-//        self.zip = attributes["zip"]
-//        self.country = attributes["country"]
-//        self.phoneNumber = attributes["phoneNumber"]
-//    }
+    
+    public init() {}
+
+    public init(attributes: [String: String]) {
+        self.firstName = attributes["firstName"]
+        self.lastName = attributes["lastName"]
+        self.number = attributes["number"]
+        self.verificationValue = attributes["verificationValue"]
+        self.month = attributes["month"]
+        self.year = attributes["year"]
+        self.address1 = attributes["address1"]
+        self.address2 = attributes["address2"]
+        self.city = attributes["city"]
+        self.state = attributes["state"]
+        self.zip = attributes["zip"]
+        self.country = attributes["country"]
+        self.phoneNumber = attributes["phoneNumber"]
+    }
 }
