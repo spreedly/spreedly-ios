3e2fb6ae9778ae13b971f8b45a4c6bd8282b307e
David Santoso <david@spreedly.com>
2015-10-26 13:11:14 -0400

    Add view classes to example

---
+++ Example/Example/CCTextField.swift
@@ -0,0 +1,9 @@
+//
+//  CCTextField.swift
+//  Example
+//
+//  Created by David Santoso on 10/26/15.
+//  Copyright © 2015 Spreedly Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Example/CCViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CreditCardViewController.swift
+//  Example
+//
+//  Created by David Santoso on 10/25/15.
+//  Copyright © 2015 Spreedly Inc. All rights reserved.
+//
+
+import UIKit
+
+class CreditCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
