14994e0a02226c84ae05060e6d984bc970998d22
David Santoso <david@spreedly.com>
2015-10-26 13:12:51 -0400

    Initial setup for simple example app

--- Example/Example/CCTextField.swift
+++ Example/Example/CCTextField.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import UIKit
+
+class CCTextField: UITextField {
+    let padding = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15);
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    private func newBounds(bounds: CGRect) -> CGRect {
+        
+        var newBounds = bounds
+        newBounds.origin.x += padding.left
+        newBounds.origin.y += padding.top
+        newBounds.size.height -= padding.top + padding.bottom
+        newBounds.size.width -= padding.left + padding.right
+        return newBounds
+    }
+}
--- Example/Example/CCViewController.swift
+++ Example/Example/CCViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import Spreedly
 
-class CreditCardViewController: UIViewController {
-
+class CCViewController: UIViewController {
+    
+    let environmentKey = ""
+    let backendChargeURL = "http://localhost:3001"
+    let appleMerchantId = "merchant.com.you-company.testing"
+    
+    @IBOutlet weak var cardNumber: CCTextField!
+    @IBOutlet weak var cardExpiration: CCTextField!
+    @IBOutlet weak var cardVerificationValue: CCTextField!
+    @IBOutlet weak var purchaseActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
@@ -21,4 +30,17 @@ class CreditCardViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleBuyNowTapped(sender: AnyObject) {
+        self.purchaseActivityIndicator.startAnimating()
+        
+        let creditCard = CreditCard()
+        creditCard.number = self.cardNumber.text!
+        creditCard.month = CreditCard.extractMonth(self.cardExpiration.text!)
+        creditCard.year = CreditCard.extractYear(self.cardExpiration.text!)
+        creditCard.verificationValue = self.cardVerificationValue.text!
+        
+        let client = SpreedlyAPIClient(environmentKey: environmentKey)
+        client.createPaymentMethodTokenWithCreditCard(creditCard) { paymentMethod, error -> Void in
+        }
+    }
 }
